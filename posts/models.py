03293c1053abc91278aa7a24749fb95a59e09eed
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-08-25 19:34:30 +0200

    add posts app , design posts model

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User 
 # Create your models here.
 '''
 
@@ -13,6 +14,8 @@ tags
 '''
 
 class Post(models.Model):
+    author = models.ForeignKey(User,related_name='post_author' , on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=120)
     content = models.TextField(max_length=30000)
     publish_date = models.DateTimeField()
+    tags = TaggableManager()
