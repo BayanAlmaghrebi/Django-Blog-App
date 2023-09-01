0ebdbe90147497ec97f521e6eb224d83c670ab96
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-09-01 20:47:53 +0200

    add blog post_list , post_detail

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post , Comment
 
-# Create your views here.
+
+
+def post_list(request): 
+    data = Post.objects.all() # list
+    return render(request,'all_post.html',{'posts':data})  # 3 (request, 'html', context{})
+
+
+
+
+def post_detail(request,post_id):
+    data = Post.objects.get(id=post_id)
+    return render(request,'post.html',{'post':data})
