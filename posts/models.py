0aaac67a29b2cf4b330360a9ecddf55d26b331d3
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-09-20 00:16:24 +0200

    add slug

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User 
 from django.utils import timezone
+from django.utils.text import slugify
 # Create your models here.
 '''
 
@@ -21,9 +22,16 @@ class Post(models.Model):
     publish_date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='posts')
     tags = TaggableManager()
+    slug = models.SlugField(null=True,blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
 class Comment(models.Model):
     user = models.ForeignKey(User,related_name='comment_author' , on_delete=models.SET_NULL,null=True)
     post = models.ForeignKey(Post,related_name='comment_post', on_delete=models.CASCADE)
     Comment = models.TextField(max_length=1000)
     create_at = models.DateTimeField(default=timezone.now)
+
+    
--- project/urls.py
+++ project/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/' , PostList.as_view()),
     path('blog/add', PostCreate.as_view()),
-    path('blog/<int:pk>' , PostDetail.as_view()),
-    path('blog/<int:pk>/edit' , PostEdit.as_view()),
-    path('blog/<int:pk>/delete' , PostDelete.as_view()),
+    path('blog/<slug:slug>' , PostDetail.as_view()),
+    path('blog/<slug:slug>/edit' , PostEdit.as_view()),
+    path('blog/<slug:slug>/delete' , PostDelete.as_view()),
 ]
 
 
